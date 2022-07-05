b5f1a32dc0b9bbb9ff64262d6dfcd9e4f9df7bbf
Shomy <61943525+ShomyKohai@users.noreply.github.com>
2022-07-05 03:12:52 +0200

    Setup

--- setup.py
+++ setup.py
@@ -8,28 +8,35 @@ with open("askitsu/__init__.py") as f:
         r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE
     ).group(1)
 
-#from discord.py
-if version.endswith(('a', 'b')) or version.startswith("0"):
+# from discord.py
+if version.endswith(("a", "b")) or version.startswith("0"):
     try:
         import subprocess
-        p = subprocess.Popen(['git', 'rev-list', '--count', 'HEAD'],
-                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        p = subprocess.Popen(
+            ["git", "rev-list", "--count", "HEAD"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
         out, err = p.communicate()
         if out:
-            version += out.decode('utf-8').strip()
-        p = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'],
-                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            version += out.decode("utf-8").strip()
+        p = subprocess.Popen(
+            ["git", "rev-parse", "--short", "HEAD"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
         out, err = p.communicate()
         if out:
-            version += '+g' + out.decode('utf-8').strip()
+            version += "+g" + out.decode("utf-8").strip()
     except Exception:
         pass
 
 readme = ""
-with open("README.md") as f:
+with open("README.md", encoding="utf-8") as f:
     readme = f.read()
 
-packages = ["askitsu"]
+packages = ["askitsu", "askitsu.models"]
 
 setup(
     name="askitsu",
@@ -40,7 +47,7 @@ setup(
     long_description=readme,
     long_description_content_type="text/markdown",
     packages=packages,
-    keywords="kitsu",
+    keywords=["kitsu", "kitsu api", "kitsu.io", "async"],
     install_requires=["aiohttp", "colorama"],
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -56,4 +63,8 @@ setup(
         "Topic :: Utilities",
         "Typing :: Typed",
     ],
+    project_urls={
+        "Documentation": "https://askitsu.rtfd.org/",
+        "Source": "https://github.com/ShomyKohai/askitsu",
+    },
 )
